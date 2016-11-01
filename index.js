c01cf752348aaf854c8e87f1a4fde0ec4debba63
jametra1999 <ceoladandridge@yahoo.com>
2016-11-01 17:52:22 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,19 @@
+var greeting = "Hello, everybody!";
+
+var specialGuest = 'Neil deGrasse Tyson!!!'
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic =  "space";
+
+var conversation = `Let's talk about space.`;
+
+var writeGreetingToDocument = `document.getElementById(\'greeting\').innerHTML = greeting;`
+
+// Type your answer into the console without the quote marks and see if it works.
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = `console.log(customCelebration);`
+
+// Next, change the style of the greeting using only JavaScript. It make the greeting have a size of 28 pixels, make it bold, make it blue,
